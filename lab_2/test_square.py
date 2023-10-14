7514e1f6c35b0bf2b4346bb1ec2fd183a501810b
patrikeevairina <patrikeevairina250901@mail.ru>
2023-10-15 00:40:21 +0300

    test: correct init

--- lab_2/test_square.py
+++ lab_2/test_square.py
@@ -1,3 +1,5 @@
+import math
+
 from square import Circle, Triangle, Rectangle
 
 
@@ -12,6 +14,18 @@ def test_circle_incorrect_init():
             assert True
 
 
+def test_circle_correct_init():
+    correct_radius = (3, 4.5, math.pi)
+    for radius in correct_radius:
+        try:
+            c = Circle(radius)
+            if not isinstance(c, Circle):
+                assert False
+        except Exception:
+            assert False
+    assert True
+
+
 def test_triangle_incorrect_init():
     incorrect_sides = ((1, 2, 3), (-2, 0, 2), (0, 0, 1), (-3, -4, -5), (-3, 4, -5))
     for sides in incorrect_sides:
@@ -23,6 +37,18 @@ def test_triangle_incorrect_init():
             assert True
 
 
+def test_triangle_correct_init():
+    correct_sides = ((3, 4, 5), (math.e, math.pi, 1))
+    for sides in correct_sides:
+        try:
+            t = Triangle(sides[0], sides[1], sides[2])
+            if not isinstance(t, Triangle):
+                assert False
+        except Exception:
+            assert False
+    assert True
+
+
 def test_rectangle_incorrect_init():
     incorrect_sides = ((0, 0), (-1, -4), (3, 4), (5, -3), (0, 2), (5, 0))
     for sides in incorrect_sides:
@@ -32,4 +58,16 @@ def test_rectangle_incorrect_init():
                 assert False
         except Exception:
             assert True
+
+
+def test_rectangle_correct_init():
+    correct_sides = ((3, 5.6), (math.e, math.pi))
+    for sides in correct_sides:
+        try:
+            r = Rectangle(sides[0], sides[1])
+            if not isinstance(r, Rectangle):
+                assert False
+        except Exception:
+            assert False
+    assert True
 
