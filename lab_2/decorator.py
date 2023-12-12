51419fce3a02bce0889d2490ad3a92248a73eae9
ptr_ia <patrikeevairina250901@mail.ru>
2023-12-12 10:42:48 +0300

    fix decorator

--- lab_2/decorator.py
+++ lab_2/decorator.py
@@ -1,14 +1,15 @@
 import time
+import abc
 import types
 
 
-class BaseDecorator:
+class BaseDecorator(abc.ABC):
     def __init__(self, func):
-        self.__func = func
-        func_ptr = func
-        while not isinstance(func_ptr, types.FunctionType):
-            func_ptr = func_ptr.__func
-        self.f_name = func_ptr.__name__
+        self._func = func
+        # func_ptr = func
+        # while not isinstance(func_ptr, types.FunctionType):
+        #     func_ptr = func_ptr.__func
+        self.__name__ = self._func.__name__
         self.history = ""  # поле, в котором хранится история
         self.time = 0
 
@@ -18,25 +19,34 @@ class BaseDecorator:
             arg_line = arg_line + "{0}, ".format(arg)
         new_line = "<{0}>: function <{1}> called with arguments <{2}>".format(start_time, func_name, arg_line)
         self.history = self.history + new_line
-        # print(new_line)
 
-    def __call__(self, *args, **kwargs):
-        start_time = time.time()
-        self.__func(*args, **kwargs)
-        self.update_history(start_time, self.f_name, *args)
-        self.time = time.time() - start_time
-        self.print_time()
-
-    def print_time(self):
-        print(self.time)
+    # def __call__(self, *args, **kwargs):
+    #     self.__func(*args, **kwargs)
+    #     self.time = time.time()
+    #     self.update_history(self.time, self.f_name, *args)
+    #     # self.print_time()
 
 
 class Decorator(BaseDecorator):
-    def print_time(self):
-        print(self.time)
+    def __init__(self, func):
+        super().__init__(func)
+
+    def __call__(self, *args, **kwargs):
+        start_time = time.time()
+        self._func(*args, **kwargs)
+        time_delta = time.time() - start_time
+        self.update_history(start_time, self._func.__name__, *args)
+        print(self.history)
+        return time_delta
 
 
 class HTMLDecorator(BaseDecorator):
+
+    def __call__(self, *args, **kwargs):
+        time_delta = self._func(*args, **kwargs)
+        self.update_history(time.time() - time_delta, self.__name__, *args)
+        print(self.history)
+        
     def print_time(self):
         html_time = "<html><body>{0}</body><html>".format(self.time)
         print(html_time)
@@ -62,6 +72,6 @@ def _mp(nums):
 
 
 num_list = [i + i % 3 for i in range(1000)]
-_for(num_list)
+# _for(num_list)
 _lst(num_list)
-_mp(num_list)
+# _mp(num_list)
